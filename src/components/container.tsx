1d5ced6c0185ebe731196a51f7c4a581a95942f9
Dawsonkw <dawsonkw@gmail.com>
2024-07-29 12:26:54 -0600

    set type for container

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,4 +1,8 @@
-function container({ children }) {
+type ContainerProps = {
+  children: React.ReactNode;
+};
+
+function container({ children }: ContainerProps) {
   return (
     <div className="flex flex-col max-w-7xl min-h-screen mx-auto bg-white/[2%]">
       {children}

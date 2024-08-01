dff820a96ce874ed1aeca146551437150d1062f2
Dawsonkw <dawsonkw@gmail.com>
2024-08-01 15:34:08 -0600

    set up the event to handle fetching the data from the API. Nextjs is so cool. also my eyes are itchy. also im gonna go make macaroni.

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -5,8 +5,15 @@ type EventsPageProps = {
   };
 };
 
-function EventsPage({ params }: EventsPageProps) {
+async function EventsPage({ params }: EventsPageProps) {
   const city = params.city;
+
+  const response = await fetch(
+    "https://bytegrad.com/course-assets/projects/evento/api/events?city=austin"
+  );
+
+  const events = await response.json();
+
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
       <H1>
@@ -14,6 +21,10 @@ function EventsPage({ params }: EventsPageProps) {
         {city !== "all" &&
           `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
       </H1>
+
+      {events.map((event) => (
+        <section key={event.id}>{event.name}</section>
+      ))}
     </main>
   );
 }

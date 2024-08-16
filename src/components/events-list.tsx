e10b27165212f140cd491e181f823c062b788179
Dawsonkw <dawsonkw@gmail.com>
2024-08-16 13:36:25 -0600

    styled the event card and event list components added in pic and infor for cards

--- src/components/events-list.tsx
+++ src/components/events-list.tsx
@@ -7,7 +7,7 @@ type EventsListProps = {
 
 function Eventslist({ events }: EventsListProps) {
   return (
-    <section>
+    <section className="flex flex-wrap gap-10 justify-center max-w-[1100px] px-[20px]">
       {events.map((event) => (
         <EventCard key={event.id} event={event} />
       ))}

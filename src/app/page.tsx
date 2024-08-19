bf5761ba73572ae418306d882fb920e2776455a9
Dawsonkw <dawsonkw@gmail.com>
2024-08-19 12:09:47 -0600

    changed endpoint of api so that it now pulls from all cities rather than just austin.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex flex-col items-center pt-36 px-3">
-      <H1>Find events around you</H1>
+      <H1 className="">Find events around you</H1>
       <p className="mb-12 mt-7 text-2xl lg:text-3xl opacity-75">
         Browse more than{" "}
         <span className="font-bold italic underline text-accent">
@@ -20,7 +20,6 @@ export default function Home() {
         <p>Popular:</p>
         <div className="space-x-2 font-semibold">
           <Link href="/events/austin">Austin</Link>
-          <Link href="/events/denver">Denver</Link>
           <Link href="/events/seattle">Seattle</Link>
         </div>
       </section>

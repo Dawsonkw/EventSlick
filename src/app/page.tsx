8030591bc7eefd37726e4b46ffc0e1abd44274a6
Dawsonkw <dawsonkw@gmail.com>
2024-08-30 09:40:24 -0600

    doing event page styling for single events.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,10 @@ export default function Home() {
     <main className="flex flex-col items-center pt-36 px-3">
       <H1 className="">Find events around you</H1>
       <p className="mb-12 mt-7 text-2xl lg:text-3xl opacity-75">
-        Browse more than{" "}
+        Browse more than
         <span className="font-bold italic underline text-accent">
           10,000 events
-        </span>{" "}
+        </span>
         around you
       </p>
 

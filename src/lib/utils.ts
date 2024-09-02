51fe77ed4668d5181ad66f52ff7b21e027f4d197
Dawsonkw <dawsonkw@gmail.com>
2024-09-02 13:58:44 -0600

    fixed some css for the image placeholder, added in a sleeper function to help even out client side caching time feel

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export async function sleep(ms: number) {
+  return new Promise((resolve, ms) => setTimeout(resolve, 1000));
+}

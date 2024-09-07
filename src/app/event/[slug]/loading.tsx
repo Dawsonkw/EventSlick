f9fadade41969916852fa365cfaab1d499ee5fcf
Dawsonkw <dawsonkw@gmail.com>
2024-09-07 08:27:39 -0600

    Learning some nextJS magic with loading components. Styled it to make it look pretty

---
+++ src/app/event/[slug]/loading.tsx
@@ -0,0 +1,9 @@
+function Loading() {
+  return (
+    <div className="text-center text-3xl text-white/75 mt-10">
+      Loading City...{" "}
+    </div>
+  );
+}
+
+export default Loading;
---
+++ src/app/events/[city]/loading.tsx
@@ -0,0 +1,9 @@
+function Loading() {
+  return (
+    <div className="mt-10 text-center text-3xl text-white/75">
+      Loading Events...{" "}
+    </div>
+  );
+}
+
+export default Loading;

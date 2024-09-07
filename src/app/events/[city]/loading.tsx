bade7d70f88de948deb270fd6bb2d3102cd8a10b
Dawsonkw <dawsonkw@gmail.com>
2024-09-07 09:01:20 -0600

    built skeleton card for loading of all cards.

--- src/app/events/[city]/loading.tsx
+++ src/app/events/[city]/loading.tsx
@@ -1,7 +1,11 @@
+import SkeletonCard from "@/components/skeleton-card";
+
 function Loading() {
   return (
-    <div className="mt-10 text-center text-3xl text-white/75">
-      Loading Events...{" "}
+    <div className="flex flex-wrap justify-center max-w-[1100px] mx-auto px-[20px] py-24 gap-20 mt-36">
+      {Array.from({ length: 6 }).map((item, i) => (
+        <SkeletonCard key={i} />
+      ))}
     </div>
   );
 }
